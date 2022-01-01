61d0a4514a71ea85f416d1f82ecddc00f1ad6053
Lo Van Long <longlv@v-auto.com.vn>
2022-01-01 17:32:19 +0700

    fix

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -14,7 +14,7 @@ const Nav = () => {
 					<svg className="fill-current h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg"><path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z"/></svg>
 					<span className="font-semibold text-xl tracking-tight">
 					<Link href="/">
-					<a className="">Cửa hàng</a>
+					<a className="">Hàng Bưu Điện</a>
 					</Link>
 					</span>
 				</div>
@@ -31,40 +31,40 @@ const Nav = () => {
 					<div className="text-sm font-medium uppercase lg:flex-grow">
 					<Link href="/categories">
 						<a className="block mt-4 lg:inline-block lg:mt-0 text-black hover:text-black mr-10">
-							Categories
+							Cửa hàng
 						</a>
 					</Link>
-					<Link href="/">
+					<Link href="https://www.facebook.com/buudientulap/about/">
 						<a className="block mt-4 lg:inline-block lg:mt-0 text-black hover:text-black mr-10">
-							Women
+							Giới thiệu
 						</a>
 					</Link>
-					<Link href="/">
+					<Link href="https://www.facebook.com/buudientulap/reviews/">
 						<a className="block mt-4 lg:inline-block lg:mt-0 text-black hover:text-black mr-10">
-							Kids
+							Đánh giá
 						</a>
 					</Link>
-					<Link href="/">
+					<Link href="https://www.facebook.com/buudientulap/events/">
 						<a className="block mt-4 lg:inline-block lg:mt-0 text-black hover:text-black mr-10">
-							Home & Living
+							Sự kiện
 						</a>
 					</Link>
-					<Link href="/">
+					<Link href="https://www.facebook.com/buudientulap/offers/">
 						<a className="block mt-4 lg:inline-block lg:mt-0 text-black hover:text-black mr-10">
-							Offers
+							Ưu đãi
 						</a>
 					</Link>
 					</div>
 
 					<div className="text-sm font-medium">
-						<a href="#responsive-header" className="block mt-4 lg:inline-block lg:mt-0 text-black hover:text-black mr-10">
+						{/* <a href="#responsive-header" className="block mt-4 lg:inline-block lg:mt-0 text-black hover:text-black mr-10">
 						<svg xmlns="http://www.w3.org/2000/svg" className="hidden lg:block m-auto" fill="none" viewBox="0 0 24 24" width="18" height="auto" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" /></svg>
 							Profile
 						</a>
 						<a href="#responsive-header" className="block mt-4 lg:inline-block lg:mt-0 text-black hover:text-black mr-10">
 						<svg xmlns="http://www.w3.org/2000/svg" className="hidden lg:block m-auto" fill="none" viewBox="0 0 24 24" width="18" height="auto" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01" /></svg>
 							Wishlist
-						</a>
+						</a> */}
 						<CartIcon/>
 					</div>
 				</div>
