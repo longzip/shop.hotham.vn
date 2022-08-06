ae982eb7ea2accd3af123c8c3c34c1b7a2a39522
Lo Van Long <longlv@v-auto.com.vn>
2022-08-06 22:44:03 +0700

    fix

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => (
 	<div className="footer bg-gray-800 p-6 text-white">
 		<div className="container mx-auto">
 			<div className="footer-text flex-none md:flex items-center justify-between">
-				<p>© Bưu điện văn hóa xã Tự Lập 2022</p>
+				<p>© Bưu điện huyện Mê Linh 2022</p>
 				<p className="text-gray">Được thiết kế bởi <a href="https://lovanlong.ga/">Lỗ Văn Long</a></p>
 				<span className="text-gray">Từ nhà bạn đến chúng tôi ngắn hơn đấy!</span>
 			</div>
