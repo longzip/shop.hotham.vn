3d5798da990a631c71c3af1f9229e59e234cd0f5
Lo Van Long <longlv@v-auto.com.vn>
2022-10-11 11:53:33 +0700

    fix

--- pages/checkout.js
+++ pages/checkout.js
@@ -1,29 +1,29 @@
 import Layout from "../src/components/Layout";
 import CheckoutForm from "../src/components/checkout/CheckoutForm";
-import GET_COUNTRIES from "../src/queries/get-countries";
-import client from "../src/components/ApolloClient";
+// import GET_COUNTRIES from "../src/queries/get-countries";
+// import client from "../src/components/ApolloClient";
 
-const Checkout = ({data}) => (
+const Checkout = () => (
 	<Layout>
 		<div className="checkout container mx-auto my-32 px-4 xl:px-0">
 			<h1 className="mb-5 text-2xl uppercase">Thanh toán</h1>
-			<CheckoutForm countriesData={data?.wooCountries ?? {}}/>
+			<CheckoutForm />
 		</div>
 	</Layout>
 );
 
 export default Checkout;
 
-export async function getStaticProps() {
-	const { data } = await client.query({
-		query: GET_COUNTRIES
-	});
+// export async function getStaticProps() {
+// 	const { data } = await client.query({
+// 		query: GET_COUNTRIES
+// 	});
 
-	return {
-		props: {
-			data: data || {}
-		},
-		revalidate: 1
-	};
+// 	return {
+// 		props: {
+// 			data: data || {}
+// 		},
+// 		revalidate: 1
+// 	};
 
-}
+// }
