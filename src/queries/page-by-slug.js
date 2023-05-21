5f9ab5756591bde37218ee48c931a1ebba1049a9
Lo Van Long <info@lovanlong.ga>
2023-05-22 05:21:26 +0700

    update

--- src/queries/page-by-slug.js
+++ src/queries/page-by-slug.js
@@ -20,7 +20,14 @@ export const PAGE_SLUGS = gql`
   query Pages {
     pages(
       where: {
-        notIn: ["cG9zdDo0", "cG9zdDozNDY=", "cG9zdDoxMA==", "cG9zdDozNTM="]
+        notIn: [
+          "cG9zdDo0"
+          "cG9zdDozNDY="
+          "cG9zdDoxMA=="
+          "cG9zdDozNTM="
+          "cG9zdDo1NQ=="
+          "cG9zdDo1"
+        ]
       }
       first: 5000
     ) {
