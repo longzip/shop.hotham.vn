755fe10f7177d0c1944d8dedc93cca9328aeadaf
Lo Van Long <info@lovanlong.ga>
2024-02-14 18:35:00 +0700

    add form

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,5 +18,8 @@ module.exports = {
       },
     },
   },
-  plugins: [require("@tailwindcss/aspect-ratio")],
+  plugins: [
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/aspect-ratio"),
+  ],
 };
